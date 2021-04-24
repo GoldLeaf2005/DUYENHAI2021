854693158eb967e07f69197ab0b30c7c5f664580
v0ct0r <namvutranngoc@gmail.com>
2021-04-24 16:22:55 +0700

    beta version for KNAPSACK.cpp (DH20172018)

--- DH201718C.cpp
+++ DH201718C.cpp
@@ -6,9 +6,11 @@ using namespace __gnu_pbds;
 
 typedef pair<int, int> ii;
 typedef pair<int, ii> ipii;
+typedef pair<int, vector<int>> pivi;
+
 int n;
-vector<ii> items;
 int capa1, capa2;
+vector<ii> items;
 
 struct hash_trio {
 	size_t operator() (const pair<int, pair<int, int>> &p) const {
@@ -19,35 +21,69 @@ struct hash_trio {
 	}
 };
 
-gp_hash_table<ipii, int, hash_trio> memo;
-int dp_double_knapsack(int i, int cap1, int cap2) {
+gp_hash_table<ipii, pivi, hash_trio> memo;
+vector<int> blank;
+pivi dp_double_knapsack(int i, int cap1, int cap2) {
 	if (cap1 < 0 || cap2 < 0) {
-		return INT_MIN;
+		return pivi(INT_MIN, blank);
 	}
 	if (i == n) {
-		return 0;
+		return pivi(0, blank);
 	}
 	ipii t = make_pair(i, make_pair(cap1, cap2));
 	auto it = memo.find(t);
 	if (it != memo.end()) {
 		return it->second;
 	}
-	int skip = dp_double_knapsack(i+1, cap1, cap2);
-	int take1 = dp_double_knapsack(i+1, cap1 - items[i].first, cap2) + items[i].second;
-	int take2 = dp_double_knapsack(i+1, cap1, cap2 - items[i].first) + items[i].second;
-	int ans = max(skip, max(take1, take2));
+	pivi skip = dp_double_knapsack(i+1, cap1, cap2);
+	pivi take1 = dp_double_knapsack(i+1, cap1 - items[i].first, cap2);
+	pivi take2 = dp_double_knapsack(i+1, cap1, cap2 - items[i].first);
+	take1.first += items[i].second;
+	take2.first += items[i].second;
+	int ans_fi = max(skip.first, max(take1.first, take2.first));
+	pivi ans;
+	ans.first = ans_fi;
+	if (ans_fi == skip.first) {
+		ans.second = skip.second;
+		(ans.second).push_back(0);
+	}
+	else if (ans_fi == take1.first) {
+		ans.second = take1.second;
+		(ans.second).push_back(1);
+	}
+	else {
+		ans.second = take2.second;
+		(ans.second).push_back(2);
+	}
 	memo[t] = ans;
 	return ans;
 }
 
+void show_vi(vector<int> &v) {
+	for (auto i : v) {
+		cout << i << " ";
+	}
+	cout << "\n";
+}
+
+void show_vi_r(vector<int> &v) {
+	int n = v.size();
+	for (int i = n-1; i >= 0; --i) {
+		cout << v[i] << " ";
+	}
+	cout << "\n";
+}
+
 signed main(void) {
+	// freopen("KNAPSACK.INP", "r", stdin);
+	// freopen("KNAPSACK.OUT", "w", stdout);
 	cin >> n >> capa1 >> capa2;
 	for (int i = 0; i < n; ++i) {
 		int a, b;
 		cin >> a >> b;
 		items.push_back(make_pair(a, b));
 	}
-	int ans = dp_double_knapsack(0, capa1, capa2);
-	cout << ans << "\n";
+	pivi ans = dp_double_knapsack(0, capa1, capa2);
+	show_vi_r(ans.second);
 	return 0;
 }
