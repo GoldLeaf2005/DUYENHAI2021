6361688d50a75741e6e032d911c0bd1376434c08
v0ct0r <namvutranngoc@gmail.com>
2021-04-30 22:08:41 +0700

    optimized to 2e8 TLE (current version) over 2e7 TLE (previous version)

--- DHTBA_v2.cpp
+++ DHTBA_v2.cpp
@@ -13,75 +13,38 @@ int find_group(int n) {
 	return countn;
 }
 
-string next_binary(string str) {
-	string one = "1";
-	int n = str.length();
-	string answer = str;
-	for (int i = n-1; i >= 0; --i) {
-		if (answer[i] == '1') {
-			answer[i] = '0';
-		}
-		else {
-			answer[i] = '1';
-			return answer;
-		}
-	}
-	one += answer;
-	return one;
-}
-
-char digits[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-void next_xary(vector<int> &vstr, int base) {
-	if (base > strlen(digits)) {
-		return ;
-	}
-	vector<int> one = {1};
+void next_xary(vector<int> &vstr) {
 	int n = vstr.size();
 	for (int i = n-1; i >= 0; --i) {
-		char c = vstr[i];
-		if (c == base-1) {
-			vstr[i] = 0;
+		if (vstr[i] == 7) {
+			vstr[i] = 4;
 		}
 		else {
-			vstr[i]++;
+			vstr[i] = 7;
 			return ;
 		}
 	}
-	one.insert(one.end(), vstr.begin(), vstr.end());
-	vstr = one;
-	return ;
 }
 
 void print_xary(vector<int> &vstr) {
 	for (auto i : vstr) {
-		cout << digits[i];
+		cout << i;
 	}
 }
 
-string nth_binary(int n, int digits) {
-	string answer(digits, '0');
-	for (int i = 0; i < n; ++i) {
-		answer = next_binary(answer);
+void nth_lucky(vector<int> &answer, int n) {
+	for (int i = 0 ; i < n; ++i) {
+		next_xary(answer);
 	}
-	return answer;
 }
 
 int n, base;
 signed main(void) {
 	cin >> n;
 	int k = find_group(n);
-	int countn = 0;
-	for (int i = 1; i < k; ++i) {
-		countn += (1 << i);
-	}
-	string answer = nth_binary(n - countn - 1, k);
-	for (int i = 0; i < k; ++i) {
-		if (answer[i] == '0') {
-			answer[i] = '4';
-		}
-		else {
-			answer[i] = '7';
-		}
-	}
-	cout << answer << "\n";
+	int countn = (1 << k) - 2;
+	vector<int> answer(k, 4);
+	nth_lucky(answer, n - countn - 1);
+	print_xary(answer);
+	cout << "\n";
 }
