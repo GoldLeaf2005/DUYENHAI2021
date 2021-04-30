ec016e4097c3000bb191812c8f0334335f40639f
v0ct0r <namvutranngoc@gmail.com>
2021-04-30 19:23:26 +0700

    program that counts up to n in base k

---
+++ NEXT_STRING_BASE_N.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int find_group(int n) {
+	int curGroup = 2;
+	int totalNumbers = 0;
+	int countn = 0;
+	while (n > totalNumbers) {
+		countn++;
+		totalNumbers += curGroup;
+		curGroup <<= 1;
+	}
+	return countn;
+}
+
+string next_binary(string str) {
+	string one = "1";
+	int n = str.length();
+	string answer = str;
+	for (int i = n-1; i >= 0; --i) {
+		if (answer[i] == '1') {
+			answer[i] = '0';
+		}
+		else {
+			answer[i] = '1';
+			return answer;
+		}
+	}
+	one += answer;
+	return one;
+}
+
+char digits[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+void next_xary(vector<int> &vstr, int base) {
+	if (base > strlen(digits)) {
+		return ;
+	}
+	vector<int> one = {1};
+	int n = vstr.size();
+	for (int i = n-1; i >= 0; --i) {
+		char c = vstr[i];
+		if (c == base-1) {
+			vstr[i] = 0;
+		}
+		else {
+			vstr[i]++;
+			return ;
+		}
+	}
+	one.insert(one.end(), vstr.begin(), vstr.end());
+	vstr = one;
+	return ;
+}
+
+void print_xary(vector<int> &vstr) {
+	for (auto i : vstr) {
+		cout << digits[i];
+	}
+}
+
+string nth_binary(int n, int digits) {
+	string answer(digits, '0');
+	for (int i = 0; i < n; ++i) {
+		answer = next_binary(answer);
+	}
+	return answer;
+}
+
+int n, base;
+signed main(void) {
+	cin >> n >> base;
+	// int dig_n = find_group(n);
+	vector<int> startStr = {0};
+	for (int i = 0; i < n; ++i) {
+		print_xary(startStr); cout << "\n";
+		next_xary(startStr, base);
+	}
+}
